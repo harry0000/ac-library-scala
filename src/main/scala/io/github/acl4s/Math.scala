48d22fe1950c28ef3a2d63220d5cfc50b10ab618
harry0000 <zan00753@nifty.ne.jp>
2025-10-12 17:08:49 +0900

    Add floorSum function

--- src/main/scala/io/github/acl4s/Math.scala
+++ src/main/scala/io/github/acl4s/Math.scala
@@ -1,6 +1,6 @@
 package io.github.acl4s
 
-import io.github.acl4s.internal.{invGcd, safeMod, Barrett, LPair}
+import io.github.acl4s.internal.{floorSumUnsigned, invGcd, safeMod, Barrett, LPair}
 
 def powMod(x: Long, n: Long, m: Int): Int = {
   assert(0L <= n && 1 <= m)
@@ -22,4 +22,26 @@ def invMod(x: Long, m: Long): Long = {
   val LPair(z, inv) = invGcd(x, m);
   assert(z == 1L)
   inv
+}
+
+def floorSum(n: Long, m: Long, a: Long, b: Long): Long = {
+  require(0L <= n && n < (1L << 32))
+  require(1L <= m && m < (1L << 32))
+
+  var ans = 0L
+  var _a = a
+  var _b = b
+
+  if (_a < 0) {
+    val a2 = safeMod(_a, m)
+    ans -= (n * (n - 1) / 2) * ((a2 - _a) / m)
+    _a = a2
+  }
+  if (_b < 0) {
+    val b2 = safeMod(_b, m)
+    ans -= n * ((b2 - _b) / m)
+    _b = b2
+  }
+
+  ans + floorSumUnsigned(n, m, _a, _b)
 }
