9ab2c9bb3009c668e03523c264a63f37e4a0a820
harry0000 <zan00753@nifty.ne.jp>
2025-10-13 09:46:33 +0900

    Add MaxFlow (MfGraph)

---
+++ src/main/scala/io/github/acl4s/MaxFlow.scala
@@ -0,0 +1,141 @@
+package io.github.acl4s
+
+import io.github.acl4s.internal.IPair
+
+import scala.collection.mutable
+import scala.util.boundary, boundary.break
+
+final case class Edge(from: Int, to: Int, cap: Long, flow: Long)
+
+final private[acl4s] case class _Edge(to: Int, rev: Int, var cap: Long)
+
+final class MfGraph(private val n: Int) {
+  private val pos: mutable.Buffer[IPair] = mutable.ArrayBuffer.empty
+  private val g: Array[mutable.Buffer[_Edge]] = Array.fill(n)(mutable.ArrayBuffer.empty)
+
+  def addEdge(from: Int, to: Int, cap: Long): Int = {
+    assert(from < n)
+    assert(to < n)
+    assert(0L <= cap)
+
+    val m = pos.size
+    pos.append(IPair(from, g(from).size))
+
+    val fromId = g(from).size
+    val toId = g(to).size + (if (from == to) { 1 }
+                             else { 0 })
+    g(from).append(_Edge(to = to, rev = toId, cap))
+    g(to).append(_Edge(to = from, rev = fromId, cap = 0L))
+
+    m
+  }
+
+  def getEdge(i: Int): Edge = {
+    assert(0 <= i && i < pos.size)
+
+    val IPair(_1, _2) = pos(i)
+    val _e = g(_1)(_2)
+    val _re = g(_e.to)(_e.rev)
+    Edge(from = _1, to = _e.to, cap = _e.cap + _re.cap, flow = _re.cap)
+  }
+
+  def edges(): Seq[Edge] = pos.indices.map(getEdge)
+
+  def changeEdge(i: Int, newCap: Long, newFlow: Long): Unit = {
+    assert(0 <= i && i < pos.size)
+    assert(0L <= newFlow && newFlow <= newCap)
+
+    val IPair(_1, _2) = pos(i)
+    val _e = g(_1)(_2)
+    val _re = g(_e.to)(_e.rev)
+    _e.cap = newCap - newFlow
+    _re.cap = newFlow
+  }
+
+  def flow(s: Int, t: Int, flowLimit: Long = Long.MaxValue): Long = {
+    assert(0 <= s && s < n)
+    assert(0 <= t && t < n)
+    assert(s != t)
+
+    val level: Array[Int] = new Array(n)
+    val iter: Array[Int] = new Array(n)
+    val queue = mutable.Queue.empty[Int]
+
+    def bfs(): Unit = {
+      java.util.Arrays.fill(level, -1)
+      level(s) = 0
+      queue.clear()
+      queue.enqueue(s)
+
+      boundary {
+        while (queue.nonEmpty) {
+          val v = queue.dequeue()
+          g(v).foreach(e => {
+            // if (e.cap == 0 || level[e.to] >= 0) continue;
+            if (e.cap != 0 && level(e.to) < 0) {
+              level(e.to) = level(v) + 1
+              // if (e.to == t) return;
+              if (e.to == t) { break(()) }
+              queue.enqueue(e.to)
+            }
+          })
+        }
+      }
+    }
+
+    def dfs(v: Int, up: Long): Long = {
+      if (v == s) { return up }
+      var res = 0L
+      val levelV = level(v)
+      var i = iter(v)
+      while (i < g(v).size) {
+        val e = g(v)(i)
+        val re = g(e.to)(e.rev)
+        // if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+        if (levelV > level(e.to) && re.cap != 0) {
+          val d = dfs(e.to, (up - res).min(re.cap))
+          // if (d <= 0) continue;
+          if (d > 0) {
+            e.cap += d
+            re.cap -= d
+            res += d
+            if (res == up) { return res }
+          }
+        }
+        i += 1
+      }
+      level(v) = n
+      res
+    }
+
+    var flow = 0L
+    while (flow < flowLimit) {
+      bfs()
+      // if (level[t] == -1) break;
+      if (level(t) < 0) { return flow }
+      java.util.Arrays.fill(iter, 0)
+      val f = dfs(t, flowLimit - flow)
+      // if (!f) break;
+      if (f == 0) { return flow }
+      flow += f
+    }
+    flow
+  }
+
+  def minCut(s: Int): collection.IndexedSeq[Boolean] = {
+    val visited = mutable.ArrayBuffer.fill(n)(false)
+    val queue = mutable.Queue.empty[Int]
+    visited(s) = true
+    queue.enqueue(s)
+    while (queue.nonEmpty) {
+      val p = queue.dequeue()
+      g(p).foreach(e => {
+        if (e.cap > 0L && !visited(e.to)) {
+          visited(e.to) = true
+          queue.enqueue(e.to)
+        }
+      })
+    }
+    visited
+  }
+}
---
+++ src/test/scala/io/github/acl4s/MaxFlowSuite.scala
@@ -0,0 +1,215 @@
+package io.github.acl4s
+
+class MaxFlowSuite extends munit.FunSuite {
+
+  /**
+   * @see https://atcoder.jp/contests/practice2/tasks/practice2_d
+   */
+  test("AtCoder Library Practice Contest D - Maxflow") {
+    val n = 3
+    val m = 3
+    val grid = Array(
+      "#..",
+      "..#",
+      "..."
+    ).map(_.toCharArray)
+
+    val s = n * m
+    val t = s + 1
+    val g = MfGraph(n * m + 2)
+
+    def toVertex(i: Int, j: Int): Int = i * m + j
+
+    for {
+      i <- 0 until n
+      j <- 0 until m
+      if grid(i)(j) != '#'
+    } {
+      if ((i + j) % 2 == 0) {
+        for {
+          (di, dj) <- List((-1, 0), (1, 0), (0, -1), (0, 1))
+          ni = i + di
+          nj = j + dj
+          if 0 <= ni && ni < n && 0 <= nj && nj < m && grid(ni)(nj) != '#'
+        } {
+          g.addEdge(toVertex(i, j), toVertex(ni, nj), 1L)
+        }
+
+        g.addEdge(s, toVertex(i, j), 1L)
+      } else {
+        g.addEdge(toVertex(i, j), t, 1L)
+      }
+    }
+
+    assertEquals(g.flow(s, t), 3L)
+
+    val expectedEdges = 3
+    val actualEdges = g.edges().count { case Edge(from, to, _, flow) => from != s && to != t && flow > 0L }
+    assertEquals(actualEdges, expectedEdges)
+  }
+
+  test("simple") {
+    val g = MfGraph(4)
+
+    assertEquals(g.addEdge(0, 1, 1L), 0)
+    assertEquals(g.addEdge(0, 2, 1L), 1)
+    assertEquals(g.addEdge(1, 3, 1L), 2)
+    assertEquals(g.addEdge(2, 3, 1L), 3)
+    assertEquals(g.addEdge(1, 2, 1L), 4)
+    assertEquals(g.flow(0, 3), 2L)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, 1L, 1L))
+    assertEquals(g.getEdge(1), Edge(0, 2, 1L, 1L))
+    assertEquals(g.getEdge(2), Edge(1, 3, 1L, 1L))
+    assertEquals(g.getEdge(3), Edge(2, 3, 1L, 1L))
+    assertEquals(g.getEdge(4), Edge(1, 2, 1L, 0L))
+
+    assertEquals(g.minCut(0), IndexedSeq(true, false, false, false))
+  }
+
+  test("not simple") {
+    val g = MfGraph(2)
+
+    assertEquals(g.addEdge(0, 1, 1L), 0)
+    assertEquals(g.addEdge(0, 1, 2L), 1)
+    assertEquals(g.addEdge(0, 1, 3L), 2)
+    assertEquals(g.addEdge(0, 1, 4L), 3)
+    assertEquals(g.addEdge(0, 1, 5L), 4)
+    assertEquals(g.addEdge(0, 0, 6L), 5)
+    assertEquals(g.addEdge(1, 1, 7L), 6)
+    assertEquals(g.flow(0, 1), 15L)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, 1L, 1L))
+    assertEquals(g.getEdge(1), Edge(0, 1, 2L, 2L))
+    assertEquals(g.getEdge(2), Edge(0, 1, 3L, 3L))
+    assertEquals(g.getEdge(3), Edge(0, 1, 4L, 4L))
+    assertEquals(g.getEdge(4), Edge(0, 1, 5L, 5L))
+
+    assertEquals(g.minCut(0), IndexedSeq(true, false))
+  }
+
+  test("cut") {
+    val g = MfGraph(3)
+
+    assertEquals(g.addEdge(0, 1, 2L), 0)
+    assertEquals(g.addEdge(1, 2, 1L), 1)
+    assertEquals(g.flow(0, 2), 1L)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, 2L, 1L))
+    assertEquals(g.getEdge(1), Edge(1, 2, 1L, 1L))
+
+    assertEquals(g.minCut(0), IndexedSeq(true, true, false))
+  }
+
+  test("twice") {
+    val g = MfGraph(3)
+
+    assertEquals(g.addEdge(0, 1, 1L), 0)
+    assertEquals(g.addEdge(0, 2, 1L), 1)
+    assertEquals(g.addEdge(1, 2, 1L), 2)
+
+    assertEquals(g.flow(0, 2), 2L)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, 1L, 1L))
+    assertEquals(g.getEdge(1), Edge(0, 2, 1L, 1L))
+    assertEquals(g.getEdge(2), Edge(1, 2, 1L, 1L))
+
+    g.changeEdge(0, 100L, 10L);
+    assertEquals(g.getEdge(0), Edge(0, 1, 100L, 10L))
+
+    assertEquals(g.flow(0, 2), 0L)
+    assertEquals(g.flow(0, 1), 90L)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, 100L, 100L))
+    assertEquals(g.getEdge(1), Edge(0, 2, 1L, 1L))
+    assertEquals(g.getEdge(2), Edge(1, 2, 1L, 1L))
+
+    assertEquals(g.flow(2, 0), 2L)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, 100L, 99L))
+    assertEquals(g.getEdge(1), Edge(0, 2, 1L, 0L))
+    assertEquals(g.getEdge(2), Edge(1, 2, 1L, 0L))
+  }
+
+  test("bound") {
+    val INF = Long.MaxValue
+    val g = MfGraph(3)
+
+    assertEquals(g.addEdge(0, 1, INF), 0)
+    assertEquals(g.addEdge(1, 0, INF), 1)
+    assertEquals(g.addEdge(0, 2, INF), 2)
+
+    assertEquals(g.flow(0, 2), INF)
+
+    assertEquals(g.getEdge(0), Edge(0, 1, INF, 0L))
+    assertEquals(g.getEdge(1), Edge(1, 0, INF, 0L))
+    assertEquals(g.getEdge(2), Edge(0, 2, INF, INF))
+  }
+
+  /**
+   * @see https://github.com/atcoder/ac-library/issues/1
+   */
+  test("self loop") {
+    val g = MfGraph(3)
+
+    assertEquals(g.addEdge(0, 0, 100L), 0)
+
+    assertEquals(g.getEdge(0), Edge(0, 0, 100L, 0L))
+  }
+
+  /**
+   * @see https://github.com/atcoder/ac-library/issues/5
+   */
+  test("invalid") {
+    val g = MfGraph(2)
+
+    interceptMessage[java.lang.AssertionError]("assertion failed") { g.flow(0, 0) }
+    interceptMessage[java.lang.AssertionError]("assertion failed") { g.flow(0, 0, 0L) }
+  }
+
+  test("stress") {
+    for (_ <- 0 until 10_000) {
+      val n = randomInt(2, 20)
+      val m = randomInt(1, 100)
+
+      val (s, t) = {
+        val s = randomInt(0, n - 2)
+        val t = randomInt(s + 1, n - 1)
+
+        if (randomBoolean()) { (t, s) }
+        else { (s, t) }
+      }
+
+      val g = MfGraph(n)
+      (0 until m).foreach(_ => {
+        val u = randomInt(0, n - 1)
+        val v = randomInt(0, n - 1)
+        val c = randomInt(0, 10_000)
+        g.addEdge(u, v, c.toLong)
+      })
+      val flow = g.flow(s, t)
+      var dual = 0L
+      val cut = g.minCut(s)
+      val vFlow = new Array[Long](n)
+      g.edges()
+        .foreach(e => {
+          vFlow(e.from) -= e.flow
+          vFlow(e.to) += e.flow
+          if (cut(e.from) && !cut(e.to)) {
+            dual += e.cap
+          }
+        })
+
+      assertEquals(flow, dual)
+      assertEquals(-flow, vFlow(s))
+      assertEquals(flow, vFlow(t))
+      for {
+        i <- 0 until n
+        if i != s && i != t
+      } {
+        assertEquals(vFlow(i), 0L)
+      }
+    }
+  }
+
+}
