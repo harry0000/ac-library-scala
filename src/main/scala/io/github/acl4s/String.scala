b9d126ad1c2a962741f460aac941f600aea07b1e
harry0000 <zan00753@nifty.ne.jp>
2025-10-18 18:06:11 +0900

    Add suffixArray & lcpArray functions

--- src/main/scala/io/github/acl4s/String.scala
+++ src/main/scala/io/github/acl4s/String.scala
@@ -1,5 +1,70 @@
 package io.github.acl4s
 
+import io.github.acl4s.internal.foreach
+
+def suffixArray(s: Array[Int], upper: Int): Array[Int] = {
+  require(0 <= upper)
+  s.foreach(v => require(0 <= v && v <= upper))
+  internal.saIs(s.clone(), upper)
+}
+
+def suffixArrayArbitrary[T](s: Array[T])(using Ordering[T]): Array[Int] = {
+  import scala.math.Ordered.orderingToOrdered
+
+  val n = s.length
+  val idx = Array.tabulate(n)(identity).sortWith((l, r) => s(l) < s(r))
+  val s2 = new Array[Int](n)
+  var now = 0
+  foreach(0 until n)(i => {
+    if (i > 0 && s(idx(i - 1)) != s(idx(i))) {
+      now += 1
+    }
+    s2(idx(i)) = now
+  })
+  internal.saIs(s2, now)
+}
+
+def suffixArray(s: String): Array[Int] = {
+  val s2 = s.toCharArray.map(_.toInt)
+  internal.saIs(s2, s2.maxOption.getOrElse(255))
+}
+
+/**
+ * Reference:
+ * T. Kasai, G. Lee, H. Arimura, S. Arikawa, and K. Park,
+ * Linear-Time Longest-Common-Prefix Computation in Suffix Arrays and Its Applications
+ */
+def lcpArrayArbitrary[T](s: Array[T], sa: Array[Int]): Array[Int] = {
+  require(s.length == sa.length)
+  val n = s.length
+  require(n >= 1)
+
+  val rnk = new Array[Int](n)
+  foreach(sa.indices)(i => {
+    assert(0 <= sa(i) && sa(i) < n)
+    rnk(sa(i)) = i
+  })
+  val lcp = new Array[Int](n - 1)
+  var h = 0
+  foreach(0 until n)(i => {
+    if (h > 0) { h -= 1 }
+    val r = rnk(i)
+    // if (rnk[i] == 0) continue;
+    if (r != 0) {
+      val j = sa(r - 1)
+      while (j + h < n && i + h < n && s(j + h) == s(i + h)) {
+        h += 1
+      }
+      lcp(r - 1) = h
+    }
+  })
+  lcp
+}
+
+def lcpArray(s: String, sa: Array[Int]): Array[Int] = {
+  lcpArrayArbitrary(s.toCharArray.map(_.toInt), sa)
+}
+
 /**
  * Reference:
  * D. Gusfield,
--- src/test/scala/io/github/acl4s/StringSuite.scala
+++ src/test/scala/io/github/acl4s/StringSuite.scala
@@ -2,6 +2,209 @@ package io.github.acl4s
 
 class StringSuite extends munit.FunSuite {
 
+  /**
+   * @see https://atcoder.jp/contests/practice2/tasks/practice2_i
+   */
+  test("AtCoder Library Practice Contest I - Number of Substrings") {
+    def solve(s: String): Long = {
+      val n = s.length
+
+      val sa = suffixArray(s)
+      val lcp = lcpArray(s, sa)
+
+      val total = n.toLong * (n + 1) / 2
+      val sum = lcp.foldLeft(0L)(_ + _)
+
+      total - sum
+    }
+
+    // Sample Input 1
+    {
+      val s = "abcbcba"
+      val expected = 21L
+
+      val actual = solve(s)
+
+      assertEquals(actual, expected)
+    }
+    // Sample Input 2
+    {
+      val s = "mississippi"
+      val expected = 53L
+
+      val actual = solve(s)
+
+      assertEquals(actual, expected)
+    }
+    // Sample Input 3
+    {
+      val s = "ababacaca"
+      val expected = 33L
+
+      val actual = solve(s)
+
+      assertEquals(actual, expected)
+    }
+    // Sample Input 4
+    {
+      val s = "aaaaa"
+      val expected = 5L
+
+      val actual = solve(s)
+
+      assertEquals(actual, expected)
+    }
+  }
+
+  private def suffixArrayNaive(s: Array[Int]): Array[Int] = {
+    val n = s.length
+    val sa = Array.tabulate(n)(identity)
+
+    import scala.math.Ordering.Implicits.seqOrdering
+
+    sa.sortInPlaceBy(i => s.slice(i, n).toIndexedSeq)
+    sa
+  }
+
+  private def lcpArrayNaive(s: Array[Int], sa: Array[Int]): Array[Int] = {
+    val n = s.length
+    assert(n > 0)
+    val lcp = new Array[Int](n - 1)
+    for (i <- 0 until n - 1) {
+      val l = sa(i)
+      val r = sa(i + 1)
+      while (l + lcp(i) < n && lcp(i) < n && s(l + lcp(i)) == s(r + lcp(i))) {
+        lcp(i) += 1
+      }
+    }
+    lcp
+  }
+
+  test("suffixArray empty") {
+    assertEquals(suffixArray("").toSeq, Seq.empty[Int])
+    assertEquals(suffixArrayArbitrary(Array.empty[Int]).toSeq, Seq.empty[Int])
+    assertEquals(suffixArrayArbitrary(Array.empty[Long]).toSeq, Seq.empty[Int])
+  }
+
+  test("suffixArray single") {
+    assertEquals(suffixArrayArbitrary(Array(0)).toSeq, Seq(0))
+    assertEquals(suffixArrayArbitrary(Array(-1)).toSeq, Seq(0))
+    assertEquals(suffixArrayArbitrary(Array(1)).toSeq, Seq(0))
+    assertEquals(suffixArrayArbitrary(Array(Int.MinValue)).toSeq, Seq(0))
+    assertEquals(suffixArrayArbitrary(Array(Int.MaxValue)).toSeq, Seq(0))
+  }
+
+  test("suffixArray SALCP naive") {
+    for (n <- 1 to 5) {
+      val m = (0 until n).foldLeft(1) { case (acc, _) => acc * 4 }
+      for (f <- 0 until m) {
+        val s = new Array[Int](n)
+        val maxC = {
+          var g = f
+          var maxC = 0
+          for (i <- 0 until n) {
+            s(i) = g % 4
+            maxC = maxC.max(s(i))
+            g /= 4
+          }
+          maxC
+        }
+
+        val sa = suffixArrayNaive(s)
+
+        assertEquals(suffixArrayArbitrary(s).toSeq, sa.toSeq)
+        assertEquals(suffixArray(s, maxC).toSeq, sa.toSeq)
+        assertEquals(lcpArrayArbitrary(s, sa).toSeq, lcpArrayNaive(s, sa).toSeq)
+      }
+    }
+    for (n <- 1 to 10) {
+      val m = (0 until n).foldLeft(1) { case (acc, _) => acc * 2 }
+      for (f <- 0 until m) {
+        val s = new Array[Int](n)
+        val maxC = {
+          var g = f
+          var maxC = 0
+          for (i <- 0 until n) {
+            s(i) = g % 2
+            maxC = maxC.max(s(i))
+            g /= 2
+          }
+          maxC
+        }
+
+        val sa = suffixArrayNaive(s)
+
+        assertEquals(suffixArrayArbitrary(s).toSeq, sa.toSeq)
+        assertEquals(suffixArray(s, maxC).toSeq, sa.toSeq)
+        assertEquals(lcpArrayArbitrary(s, sa).toSeq, lcpArrayNaive(s, sa).toSeq)
+      }
+    }
+  }
+
+  test("suffixArray all A test") {
+    for (n <- 1 to 100) {
+      val s = Array.fill(n)(10)
+      assertEquals(suffixArrayArbitrary(s).toSeq, suffixArrayNaive(s).toSeq)
+      assertEquals(suffixArray(s, 10).toSeq, suffixArrayNaive(s).toSeq)
+      assertEquals(suffixArray(s, 12).toSeq, suffixArrayNaive(s).toSeq)
+    }
+  }
+
+  test("suffixArray all AB test") {
+    for (n <- 1 to 100) {
+      val s = Array.tabulate(n)(_ % 2)
+      assertEquals(suffixArrayArbitrary(s).toSeq, suffixArrayNaive(s).toSeq)
+      assertEquals(suffixArray(s, 3).toSeq, suffixArrayNaive(s).toSeq)
+    }
+    for (n <- 1 to 100) {
+      val s = Array.tabulate(n)(i => 1 - (i % 2))
+      assertEquals(suffixArrayArbitrary(s).toSeq, suffixArrayNaive(s).toSeq)
+      assertEquals(suffixArray(s, 3).toSeq, suffixArrayNaive(s).toSeq)
+    }
+  }
+
+  test("suffixArray string") {
+    val s = "missisippi"
+
+    val sa = suffixArray(s)
+
+    // format: off
+    val answer = IndexedSeq(
+      "i",           // 9
+      "ippi",        // 6
+      "isippi",      // 4
+      "issisippi",   // 1
+      "missisippi",  // 0
+      "pi",          // 8
+      "ppi",         // 7
+      "sippi",       // 5
+      "sisippi",     // 3
+      "ssisippi",    // 2
+    )
+    // format: on
+
+    assertEquals(sa.length, answer.size)
+
+    sa.indices.foreach(i => {
+      assertEquals(s.substring(sa(i)), answer(i))
+    })
+  }
+
+  test("lcpArray") {
+    val s = "aab"
+    val sa = suffixArray(s)
+    assertEquals(sa.toSeq, Seq(0, 1, 2))
+
+    val lcp = lcpArray(s, sa).toSeq
+    assertEquals(lcp, Seq(1, 0))
+
+    assertEquals(lcpArrayArbitrary(Array(0, 0, 1), sa).toSeq, lcp)
+    assertEquals(lcpArrayArbitrary(Array(-100, -100, 100), sa).toSeq, lcp)
+    assertEquals(lcpArrayArbitrary(Array(Byte.MinValue, Byte.MinValue, Byte.MaxValue), sa).toSeq, lcp)
+    assertEquals(lcpArrayArbitrary(Array(Int.MinValue, Int.MinValue, Int.MaxValue), sa).toSeq, lcp)
+    assertEquals(lcpArrayArbitrary(Array(Long.MinValue, Long.MinValue, Long.MaxValue), sa).toSeq, lcp)
+  }
+
   test("zAlgorithm") {
     {
       val str = "abracadabra";
