ab1ae5280eb5f54723a13c8d3b9198e0c5f5c64c
harry0000 <zan00753@nifty.ne.jp>
2025-10-26 14:07:02 +0900

    Add Scaladoc comment to IPair case class

--- src/main/scala/io/github/acl4s/internal/Tuple.scala
+++ src/main/scala/io/github/acl4s/internal/Tuple.scala
@@ -1,3 +1,8 @@
 package io.github.acl4s.internal
 
+/**
+ * Int Tuple for internal use
+ *
+ * @see [[https://github.com/scala/scala3/discussions/18044]]
+ */
 final private[acl4s] case class IPair(a: Int, b: Int)
