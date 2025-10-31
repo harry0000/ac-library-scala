79ad5684f313960c5847b32025b694821d86dc79
harry0000 <zan00753@nifty.ne.jp>
2025-10-31 14:41:45 +0900

    Refactor FastScanner to use named constants

--- example/src/main/scala/example/util/FastScanner.scala
+++ example/src/main/scala/example/util/FastScanner.scala
@@ -7,11 +7,14 @@ final class FastScanner(private val in: InputStream) {
   private var ptr = 0
   private var len = 0
 
+  private val EndOfStream = -1
+  private val Space = 0x20
+
   private def readByte(): Int = {
     if (ptr >= len) {
       len = in.read(buffer)
       ptr = 0
-      if (len <= 0) { return -1 }
+      if (len <= 0) { return EndOfStream }
     }
     val res = buffer(ptr) & 0xff
     ptr += 1
@@ -20,7 +23,7 @@ final class FastScanner(private val in: InputStream) {
 
   private def skipWhitespace(): Int = {
     var b = readByte()
-    while (b != -1 && b <= 32) {
+    while (b != EndOfStream && b <= Space) {
       b = readByte()
     }
     b
@@ -29,7 +32,7 @@ final class FastScanner(private val in: InputStream) {
   def next(): String = {
     val sb = new java.lang.StringBuilder
     var b = skipWhitespace()
-    while (b != -1 && b > 32) {
+    while (b != EndOfStream && b > Space) {
       sb.append(b.toChar)
       b = readByte()
     }
@@ -44,7 +47,7 @@ final class FastScanner(private val in: InputStream) {
       b = readByte()
     }
     var num = 0
-    while (b != -1 && b > 32) {
+    while (b != EndOfStream && b > Space) {
       num = num * 10 + (b - '0')
       b = readByte()
     }
@@ -59,7 +62,7 @@ final class FastScanner(private val in: InputStream) {
       b = readByte()
     }
     var num = 0L
-    while (b != -1 && b > 32) {
+    while (b != EndOfStream && b > Space) {
       num = num * 10 + (b - '0')
       b = readByte()
     }
@@ -69,7 +72,7 @@ final class FastScanner(private val in: InputStream) {
   def nextLine(): String = {
     val sb = new java.lang.StringBuilder
     var b = readByte()
-    while (b != -1 && b != '\n') {
+    while (b != EndOfStream && b != '\n') {
       if (b != '\r') {
         sb.append(b.toChar)
       }
